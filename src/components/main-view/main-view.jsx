b3c26ad541712fd3b1279f155a4fd8b806659ff9
logicless-bt <benpturner01@gmail.com>
2024-12-04 20:02:29 -0500

    PropTypes
    -added proptypes dependency

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { useState } from "react";
+import { useState, effectState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
+    useEffect(() => {
+      fetch("https://qfilms-e3cad25d1fad.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            director: director.author_name?.[0],
+            genre: doc.genre
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+    }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
     if(selectedMovie){
