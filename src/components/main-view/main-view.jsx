2b55b473680dee83e4e9797a935ab7f6785b609c
logicless-bt <benpturner01@gmail.com>
2024-12-16 12:37:47 -0500

    LoginView created
    -added LoginView funcitonality

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(null);
     useEffect(() => {
       fetch("https://qfilms-e3cad25d1fad.herokuapp.com/movies")
       .then((response) => response.json())
@@ -23,7 +26,10 @@ export const MainView = () => {
       });
     }, []);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    if(!user){
+      return <LoginView/>;
+    }
+
     if(selectedMovie){
         return (
         <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
